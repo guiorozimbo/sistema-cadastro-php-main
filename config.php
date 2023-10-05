3dcfff8e270b50d2dbf8c0205229aaded1a4035b
Guilherme <guilhermegv890@gmail.com>
2023-10-05 16:25:53 -0300

    Sistem cadastre PHP main complete

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'root';
+    $dbName = 'form-cad-php';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    
+?>
